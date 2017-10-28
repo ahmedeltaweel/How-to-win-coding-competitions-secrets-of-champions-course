ee1254047ae1e180b11a93c840ab4441e386085e
Ahmed Taweel <ahmed.m.eltaweel@gmail.com>
2017-10-28 20:41:02 +0200

    generate tests

--- week-1/Generate-Tests/main.py
+++ week-1/Generate-Tests/main.py
@@ -1,19 +1,29 @@
-import math
-with open('input.txt', 'r') as input:
-    a = [int(x) for x in input.readline().split(' ')]
+f = open('input.txt', 'r')
+k = int(f.readline().strip())
+f.close()
 
-_max = 0
-for n in range(2, a[0] + 1):
-    _max_in = 0
-    i = 2
-    while i < math.sqrt(a[0]):
-        if n % i == 0:
-            _max_in += 1
-            if not i == (n / i):
-                _max_in += 1
-        i += 1
-    if _max_in > _max:
-        _max = _max_in
-with open('output.txt', 'w') as output:
-    output.write(str(_max+2))
-    output.write('\n')
+dc = 1
+hcn = 1
+
+for a in range(0, 10):  # 2
+    for b in range(0, 10):  # 3
+        for c in range(0, 6):  # 5
+            for d in range(0, 4):  # 7
+                for e in range(0, 3):  # 11
+                    for f in range(0, 3):  # 13
+                        for g in range(0, 2):  # 17
+                            number = (2 ** a) * (3 ** b) * (5 ** c) * \
+                                    (7 ** d) * (11 ** e) * \
+                                    (13 ** f) * (17 ** g)
+                            if number <= k:
+                                divisors = (a + 1) * (b + 1) * (c + 1) * \
+                                        (d + 1) * (e + 1) * (f + 1) * (g + 1)
+                                if dc < divisors:
+                                    dc = divisors
+                                    hcn = number
+                                elif dc == divisors and number < hcn:
+                                    hcn = number
+
+f = open('output.txt', 'w')
+f.write(str(k - hcn + 1) + '\n')
+f.close()
